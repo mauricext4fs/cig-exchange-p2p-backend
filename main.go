73af03e0fdaa76f3f4fd1e7b857cfed80d6389c2
Maurice Courtois <mc@courtoisconsulting.com>
2018-12-22 02:00:54 +0100

    - Replace all tabs to 4 spaces - Now port 80 is static for all backends

--- main.go
+++ main.go
@@ -1,39 +1,35 @@
 package main
 
 import (
-	"cig-exchange-sso-backend/app"
-	"cig-exchange-sso-backend/controllers"
-	"fmt"
-	"net/http"
-	"os"
-
-	"github.com/gorilla/mux"
+    "cig-exchange-sso-backend/app"
+    "cig-exchange-sso-backend/controllers"
+    "fmt"
+    "net/http"
+    "github.com/gorilla/mux"
 )
 
 func main() {
 
-	router := mux.NewRouter()
+    router := mux.NewRouter()
 
-	router.HandleFunc("/api/ping", controllers.Ping).Methods("GET")
-	router.HandleFunc("/api/user/new", controllers.CreateAccount).Methods("POST")
-	router.HandleFunc("/api/user/login", controllers.Authenticate).Methods("POST")
-	router.HandleFunc("/api/contacts/new", controllers.CreateContact).Methods("POST")
-	router.HandleFunc("/api/me/contacts", controllers.GetContactsFor).Methods("GET") //  user/2/contacts
+    router.HandleFunc("/api/ping", controllers.Ping).Methods("GET")
+    router.HandleFunc("/api/user/new", controllers.CreateAccount).Methods("POST")
+    router.HandleFunc("/api/user/login", controllers.Authenticate).Methods("POST")
+    router.HandleFunc("/api/contacts/new", controllers.CreateContact).Methods("POST")
+    router.HandleFunc("/api/me/contacts", controllers.GetContactsFor).Methods("GET") //  user/2/contacts
 
-	//attach JWT auth middleware
-	router.Use(app.JwtAuthentication)
+    //attach JWT auth middleware
+    router.Use(app.JwtAuthentication)
 
-	//router.NotFoundHandler = app.NotFoundHandler
+    //router.NotFoundHandler = app.NotFoundHandler
 
-	port := os.Getenv("SSO_BACKEND_PORT")
-	if port == "" {
-		port = "8000" //localhost
-	}
+    // We always run in docker... for sack of convenience let's always use port 80
+    port := "80"
 
-	fmt.Println(port)
+    fmt.Println(port)
 
-	err := http.ListenAndServe(":"+port, router) //Launch the app, visit localhost:8000/api
-	if err != nil {
-		fmt.Print(err)
-	}
+    err := http.ListenAndServe(":"+port, router) //Launch the app, visit localhost:8000/api
+    if err != nil {
+        fmt.Print(err)
+    }
 }
