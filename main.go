f19c7eb94ae7fed46285a2ecc93972bf0b59a4d9
Maurice Courtois <mc@foxstone.ch>
2018-12-23 23:27:13 +0100

    - Remove auth.go... we are not going to authenticate through go but directly on the reverse proxy

--- main.go
+++ main.go
@@ -1,33 +1,32 @@
 package main
 
 import (
-    "cig-exchange-p2p-backend/app"
-    "cig-exchange-p2p-backend/controllers"
-    "fmt"
-    "net/http"
-    "github.com/gorilla/mux"
+	"cig-exchange-p2p-backend/controllers"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
 )
 
 func main() {
 
-    router := mux.NewRouter()
+	router := mux.NewRouter()
 
-    router.HandleFunc("/api/ping", controllers.Ping).Methods("GET")
-    router.HandleFunc("/api/contacts/new", controllers.CreateContact).Methods("POST")
-    router.HandleFunc("/api/me/contacts", controllers.GetContactsFor).Methods("GET") //  user/2/contacts
+	router.HandleFunc("/api/ping", controllers.Ping).Methods("GET")
+	router.HandleFunc("/api/contacts/new", controllers.CreateContact).Methods("POST")
+	router.HandleFunc("/api/me/contacts", controllers.GetContactsFor).Methods("GET") //  user/2/contacts
 
-    //attach JWT auth middleware
-    router.Use(app.JwtAuthentication)
+	//attach JWT auth middleware
+	//router.Use(app.JwtAuthentication)
 
-    //router.NotFoundHandler = app.NotFoundHandler
+	//router.NotFoundHandler = app.NotFoundHandler
 
-    // We always run in docker... for sack of convenience let's always use port 80
-    port := "80"
+	// We always run in docker... for sack of convenience let's always use port 80
+	port := "80"
 
-    fmt.Println(port)
+	fmt.Println(port)
 
-    err := http.ListenAndServe(":"+port, router) //Launch the app, visit localhost:8000/api
-    if err != nil {
-        fmt.Print(err)
-    }
+	err := http.ListenAndServe(":"+port, router) //Launch the app, visit localhost:8000/api
+	if err != nil {
+		fmt.Print(err)
+	}
 }
