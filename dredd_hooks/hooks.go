781ba0e774c71bb03ee9fe66d0f30c310a70b7b1
alexgblr <alexgblr>
2019-02-12 12:04:53 +0300

    fix dredd hooks; use presaved jwt for auth

--- dredd_hooks/hooks.go
+++ dredd_hooks/hooks.go
@@ -39,112 +39,25 @@ func main() {
 	// save created record UUID here
 	createdUUID := ""
 
-	// save created user UUID and JWT
-	userUUID := ""
+	// user JWT saved by homepage dredd tests
 	userJWT := ""
+	jwtCmd := client.Get("jwt")
+	if jwtCmd.Err() != nil {
+		log.Printf("Unable to get jwt from redis: Error: %v", jwtCmd.Err().Error())
+		return
+	}
+	userJWT = jwtCmd.Val()
 
-	h.After("Users > p2p/api/users/signin > Signin", func(t *trans.Transaction) {
-		// happens when api is down
-		if t.Real == nil {
+	// inject JWT auth into all calls
+	h.BeforeEach(func(t *trans.Transaction) {
+		if len(userJWT) == 0 || t.Request == nil {
 			return
 		}
 
-		body := map[string]interface{}{}
-
-		err := json.Unmarshal([]byte(t.Real.Body), &body)
-		if err != nil {
-			log.Printf("Signin: Error: %v", err.Error())
-			return
-		}
-
-		createdUUIDInterface, ok := body["uuid"]
-		if !ok {
-			log.Printf("Signin: Error: Unable to save signin uuid")
-			return
-		}
-		userUUID = createdUUIDInterface.(string)
+		t.Request.Headers["Authorization"] = fmt.Sprintf("Bearer %v", userJWT)
 	})
 
-	h.Before("Users > p2p/api/users/send_otp > Send OTP", func(t *trans.Transaction) {
-
-		if len(userUUID) == 0 || t.Request == nil {
-			return
-		}
-
-		body := map[string]interface{}{}
-
-		err := json.Unmarshal([]byte(t.Request.Body), &body)
-		if err != nil {
-			log.Printf("Send OTP: Error: %v", err.Error())
-			return
-		}
-
-		body["uuid"] = userUUID
-		b, err := json.Marshal(body)
-		if err != nil {
-			log.Printf("Send OTP: Error: %v", err.Error())
-			return
-		}
-
-		t.Request.Body = string(b)
-	})
-
-	h.Before("Users > p2p/api/users/verify_otp > Verify OTP", func(t *trans.Transaction) {
-
-		if len(userUUID) == 0 || t.Request == nil {
-			return
-		}
-
-		body := map[string]interface{}{}
-
-		err := json.Unmarshal([]byte(t.Request.Body), &body)
-		if err != nil {
-			log.Printf("Verify OTP: Error: %v", err.Error())
-			return
-		}
-
-		rediskey := fmt.Sprintf("%s_signup_key", userUUID)
-		redisCmd := client.Get(rediskey)
-		if redisCmd.Err() != nil {
-			log.Printf("Verify OTP: Error: %v", redisCmd.Err().Error())
-			return
-		}
-
-		body["uuid"] = userUUID
-		body["code"] = redisCmd.Val()
-		b, err := json.Marshal(body)
-		if err != nil {
-			log.Printf("Verify OTP: Error: %v", err.Error())
-			return
-		}
-
-		t.Request.Body = string(b)
-	})
-
-	h.After("Users > p2p/api/users/verify_otp > Verify OTP", func(t *trans.Transaction) {
-
-		// happens when api is down
-		if t.Real == nil {
-			return
-		}
-
-		body := map[string]interface{}{}
-
-		err := json.Unmarshal([]byte(t.Real.Body), &body)
-		if err != nil {
-			log.Printf("Verify OTP: Error: %v", err.Error())
-			return
-		}
-		userJWTInterface, ok := body["jwt"]
-		if !ok {
-			log.Printf("Verify OTP: Error: Unable to save user jwt")
-			return
-		}
-		userJWT = userJWTInterface.(string)
-		log.Printf("jwt: %v", userJWT)
-	})
-
-	h.After("Offerings > api/offerings > Create offering", func(t *trans.Transaction) {
+	h.After("Offerings > p2p/api/offerings > Create offering", func(t *trans.Transaction) {
 		// happens when api is down
 		if t.Real == nil {
 			return
@@ -162,21 +75,21 @@ func main() {
 	})
 
 	// update URI everywhere to point to a created record
-	h.Before("Offerings > api/offerings/{offering_id} > Retrieve offering", func(t *trans.Transaction) {
+	h.Before("Offerings > p2p/api/offerings/{offering_id} > Retrieve offering", func(t *trans.Transaction) {
 		if len(createdUUID) > 0 {
 			t.Request.URI = "/api/offerings/" + createdUUID
 			t.FullPath = "/api/offerings/" + createdUUID
 		}
 	})
 
-	h.Before("Offerings > api/offerings/{offering_id} > Update offering", func(t *trans.Transaction) {
+	h.Before("Offerings > p2p/api/offerings/{offering_id} > Update offering", func(t *trans.Transaction) {
 		if len(createdUUID) > 0 {
 			t.Request.URI = "/api/offerings/" + createdUUID
 			t.FullPath = "/api/offerings/" + createdUUID
 		}
 	})
 
-	h.Before("Offerings > api/offerings/{offering_id} > Delete offering", func(t *trans.Transaction) {
+	h.Before("Offerings > p2p/api/offerings/{offering_id} > Delete offering", func(t *trans.Transaction) {
 		if len(createdUUID) > 0 {
 			t.Request.URI = "/api/offerings/" + createdUUID
 			t.FullPath = "/api/offerings/" + createdUUID
