3ed4ad4881163153194abcea3d84f58161256b5d
alexgblr <alexgblr>
2019-03-14 17:05:07 +0300

    api description and hooks

--- dredd_hooks/hooks.go
+++ dredd_hooks/hooks.go
@@ -7,6 +7,7 @@ import (
 
 	"cig-exchange-libs"
 
+	"github.com/lib/pq"
 	"github.com/snikch/goodman/hooks"
 	trans "github.com/snikch/goodman/transaction"
 )
@@ -20,45 +21,182 @@ func main() {
 	server := hooks.NewServer(hooks.NewHooksRunner(h))
 
 	redisClient := cigExchange.GetRedis()
+	dbClient := cigExchange.GetDB()
+
+	// save created user UUID, JWT and organization UUID
+	userUUID := ""
+	userJWT := ""
+	orgUUID := ""
 
 	// save created record UUID here
 	createdUUID := ""
-	// user JWT saved by homepage dredd tests
-	userJWT := ""
-	// user organisation UUID saved by homepage dredd tests
-	orgUUID := ""
 
-	jwtCmd := redisClient.Get("jwt")
-	if jwtCmd.Err() != nil {
-		fmt.Println("ERROR: Unable to get jwt from redis:")
-		fmt.Println(jwtCmd.Err().Error())
-	} else {
-		userJWT = jwtCmd.Val()
+	// prepare the database:
+	// 1. delete 'dredd' users if it exists (first name  = 'dredd')
+	// 2. delete 'dredd' organisations if it exists (reference key = 'dredd')
+	// 3. create 'dredd' organisation (user will be registered with it)
+	// 4. create some offerings belonging to 'dredd' organization
+	// 5. verify that created offerings are present in 'invest/offerings' api call
+
+	// delete 'dredd' users if it exists (first name  = 'dredd')
+	usersDelete := make([]models.User, 0)
+	err := dbClient.Where(&models.User{Name: dredd}).Find(&usersDelete).Error
+	if err == nil {
+		for _, u := range usersDelete {
+			dbClient.Delete(&u)
+		}
 	}
 
-	orgCmd := redisClient.Get("org")
-	if orgCmd.Err() != nil {
-		fmt.Println("ERROR: Unable to get organisation UUID from redis:")
-		fmt.Println(orgCmd.Err().Error())
-	} else {
-		orgUUID = orgCmd.Val()
+	// delete 'dredd' organisations if it exists (reference key = 'dredd')
+	orgsDelete := make([]models.Organisation, 0)
+	err = dbClient.Where(&models.Organisation{ReferenceKey: dredd}).Find(&orgsDelete).Error
+	if err == nil {
+		for _, o := range orgsDelete {
+			dbClient.Delete(&o)
+		}
 	}
 
-	// inject JWT auth into all calls
+	// create 'dredd' organisation
+	org := models.Organisation{
+		Name:         dredd,
+		ReferenceKey: dredd,
+	}
+	err = dbClient.Create(&org).Error
+	if err != nil {
+		fmt.Println("ERROR: prepareDatabase: create organisation:")
+		fmt.Println(err.Error())
+	}
+	orgUUID = org.ID
+
+	// create some offerings belonging to 'dredd' organization
+	offering := models.Offering{
+		Title:          dredd,
+		OrganisationID: orgUUID,
+		Type:           make(pq.StringArray, 0),
+		IsVisible:      true,
+	}
+	err = dbClient.Create(&offering).Error
+	if err != nil {
+		fmt.Println("ERROR: prepareDatabase: create offering:")
+		fmt.Println(err.Error())
+	}
+
+	// inject JWT auth into api calls (if JWT is present)
 	h.BeforeEach(func(t *trans.Transaction) {
 		if t.Request == nil {
 			return
 		}
 
 		if len(userJWT) == 0 {
-			t.Fail = "JWT missing"
+			return
 		}
 
 		t.Request.Headers["Authorization"] = fmt.Sprintf("Bearer %v", userJWT)
 	})
 
+	h.After("Offerings > invest/api/offerings > Offerings", func(t *trans.Transaction) {
+
+		// happens when api is down
+		if t.Real == nil {
+			return
+		}
+
+		// verify that created offerings are present in 'invest/offerings' api call
+		offerings := make([]models.Offering, 0)
+		err := json.Unmarshal([]byte(t.Real.Body), &offerings)
+		if err != nil {
+			t.Fail = fmt.Sprintf("Unable to parse response: %v", err.Error())
+			return
+		}
+
+		for _, offering := range offerings {
+			if offering.Title == dredd {
+				// we found a match, api works fine
+				return
+			}
+		}
+
+		t.Fail = "Pre-created offering is missing"
+	})
+
+	h.Before("Users > invest/api/users/signup > Signup", func(t *trans.Transaction) {
+
+		if t.Request == nil {
+			return
+		}
+		if len(orgUUID) == 0 {
+			t.Fail = "Organisation UUID missing"
+			return
+		}
+
+		setBodyValue(&t.Request.Body, "reference_key", dredd)
+		setBodyValue(&t.Request.Body, "name", dredd)
+	})
+
+	h.After("Users > invest/api/users/signin > Signin", func(t *trans.Transaction) {
+
+		// happens when api is down
+		if t.Real == nil {
+			return
+		}
+
+		userUUID = getBodyValue(&t.Real.Body, "uuid")
+		if len(userUUID) == 0 {
+			t.Fail = "Unable to save user UUID"
+			return
+		}
+	})
+
+	h.Before("Users > invest/api/users/send_otp > Send OTP", func(t *trans.Transaction) {
+
+		if t.Request == nil {
+			return
+		}
+		if len(userUUID) == 0 {
+			t.Fail = "User UUID missing"
+			return
+		}
+
+		setBodyValue(&t.Request.Body, "uuid", userUUID)
+	})
+
+	h.Before("Users > invest/api/users/verify_otp > Verify OTP", func(t *trans.Transaction) {
+
+		if t.Request == nil {
+			return
+		}
+		if len(userUUID) == 0 {
+			t.Fail = "User UUID missing"
+			return
+		}
+
+		rediskey := fmt.Sprintf("%s_signup_key", userUUID)
+		redisCmd := redisClient.Get(rediskey)
+		if redisCmd.Err() != nil {
+			t.Fail = fmt.Sprintf("Redis error: %v", redisCmd.Err().Error())
+			return
+		}
+
+		setBodyValue(&t.Request.Body, "uuid", userUUID)
+		setBodyValue(&t.Request.Body, "code", redisCmd.Val())
+	})
+
+	h.After("Users > invest/api/users/verify_otp > Verify OTP", func(t *trans.Transaction) {
+
+		// happens when api is down
+		if t.Real == nil {
+			return
+		}
+
+		userJWT = getBodyValue(&t.Real.Body, "jwt")
+		if len(userJWT) == 0 {
+			t.Fail = "Unable to save user JWT"
+			return
+		}
+	})
+
 	// update URI everywhere to point to a created record
-	h.Before("Offerings > p2p/api/organisations/{organisation_id}/offerings > Create offering", func(t *trans.Transaction) {
+	h.Before("Offerings > p2p/api/organisations/{organisation}/offerings > Create offering", func(t *trans.Transaction) {
 		if t.Request == nil {
 			return
 		}
@@ -73,7 +211,7 @@ func main() {
 		setBodyValue(&t.Request.Body, "organisation_id", orgUUID)
 	})
 
-	h.After("Offerings > p2p/api/organisations/{organisation_id}/offerings > Create offering", func(t *trans.Transaction) {
+	h.After("Offerings > p2p/api/organisations/{organisation}/offerings > Create offering", func(t *trans.Transaction) {
 		// happens when api is down
 		if t.Real == nil {
 			return
@@ -86,7 +224,7 @@ func main() {
 		}
 	})
 
-	h.Before("Offerings > p2p/api/organisations/{organisation_id}/offerings > Retrieve all offerings", func(t *trans.Transaction) {
+	h.Before("Offerings > p2p/api/organisations/{organisation}/offerings > Retrieve all offerings", func(t *trans.Transaction) {
 		if t.Request == nil {
 			return
 		}
@@ -99,7 +237,7 @@ func main() {
 		t.FullPath = "/p2p/api/organisations/" + orgUUID + "/offerings"
 	})
 
-	h.After("Offerings > p2p/api/organisations/{organisation_id}/offerings > Retrieve all offerings", func(t *trans.Transaction) {
+	h.After("Offerings > p2p/api/organisations/{organisation}/offerings > Retrieve all offerings", func(t *trans.Transaction) {
 		// happens when api is down
 		if t.Real == nil {
 			return
@@ -135,7 +273,7 @@ func main() {
 		t.Fail = "Pre-created offering is missing"
 	})
 
-	h.Before("Offerings > p2p/api/organisations/{organisation_id}/offerings/{offering_id} > Retrieve offering", func(t *trans.Transaction) {
+	h.Before("Offerings > p2p/api/organisations/{organisation}/offerings/{offering} > Retrieve offering", func(t *trans.Transaction) {
 		if t.Request == nil {
 			return
 		}
@@ -152,7 +290,7 @@ func main() {
 		t.FullPath = "/p2p/api/organisations/" + orgUUID + "/offerings/" + createdUUID
 	})
 
-	h.Before("Offerings > p2p/api/organisations/{organisation_id}/offerings/{offering_id} > Update offering", func(t *trans.Transaction) {
+	h.Before("Offerings > p2p/api/organisations/{organisation}/offerings/{offering} > Update offering", func(t *trans.Transaction) {
 		if t.Request == nil {
 			return
 		}
@@ -171,7 +309,7 @@ func main() {
 		setBodyValue(&t.Request.Body, "organisation_id", orgUUID)
 	})
 
-	h.Before("Offerings > p2p/api/organisations/{organisation_id}/offerings/{offering_id} > Delete offering", func(t *trans.Transaction) {
+	h.Before("Offerings > p2p/api/organisations/{organisation}/offerings/{offering} > Delete offering", func(t *trans.Transaction) {
 		if t.Request == nil {
 			return
 		}
