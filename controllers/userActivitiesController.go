a11a12cdcd6c13106bc2fef9e4c6cc18b37a7c39
alexgblr <alexgblr>
2019-04-01 21:57:52 +0300

    add GET user activities api call

--- controllers/userActivitiesController.go
+++ controllers/userActivitiesController.go
@@ -6,7 +6,52 @@ import (
 	"cig-exchange-libs/models"
 	"encoding/json"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
+// GetUserActivities handles GET users/{user_id}/activities endpoints
+var GetUserActivities = func(w http.ResponseWriter, r *http.Request) {
+
+	// create user activity record and print error with defer
+	apiErrorP, loggedInUserP := auth.PrepareActivityVariables()
+	defer auth.CreateUserActivity(loggedInUserP, apiErrorP, models.ActivityTypeGetUserActivities)
+	defer cigExchange.PrintAPIError(apiErrorP)
+
+	// get request params
+	userID := mux.Vars(r)["user_id"]
+
+	// load context user info
+	loggedInUser, err := auth.GetContextValues(r)
+	if err != nil {
+		*apiErrorP = cigExchange.NewRoutingError(err)
+		cigExchange.RespondWithAPIError(w, *apiErrorP)
+		return
+	}
+	*loggedInUserP = loggedInUser
+
+	// check user id
+	if len(userID) == 0 {
+		*apiErrorP = cigExchange.NewInvalidFieldError("user_id", "UserID is invalid")
+		cigExchange.RespondWithAPIError(w, *apiErrorP)
+		return
+	}
+
+	if userID != loggedInUser.UserUUID {
+		*apiErrorP = cigExchange.NewAccessRightsError("No access rights for the user")
+		cigExchange.RespondWithAPIError(w, *apiErrorP)
+		return
+	}
+
+	userActs, apiError := models.GetActivitiesForUser(userID)
+	if apiError != nil {
+		*apiErrorP = apiError
+		cigExchange.RespondWithAPIError(w, *apiErrorP)
+		return
+	}
+
+	cigExchange.Respond(w, userActs)
+}
 
 // CreateUserActivity handles POST users/activities and users/{user_id}/activities endpoints
 var CreateUserActivity = func(w http.ResponseWriter, r *http.Request) {
