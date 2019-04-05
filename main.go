f2cb0eeeb1b1dae34bf8b5a70437dd21d94623a8
alexgblr <alexgblr>
2019-04-05 13:29:29 +0300

    add jwt ping api call

--- main.go
+++ main.go
@@ -36,6 +36,7 @@ func main() {
 	// register handlers for both platforms
 	// p2p
 	router.HandleFunc(p2pBaseURI+"me/info", userAPI.GetInfo).Methods("GET")
+	router.HandleFunc(p2pBaseURI+"ping-jwt", userAPI.PingJWT).Methods("GET")
 	router.HandleFunc(p2pBaseURI+"users/switch/{organisation_id}", userAPI.ChangeOrganisationHandler).Methods("POST")
 	router.HandleFunc(p2pBaseURI+"users/{user_id}", controllers.GetUser).Methods("GET")
 	router.HandleFunc(p2pBaseURI+"users/{user_id}", controllers.UpdateUser).Methods("PATCH")
